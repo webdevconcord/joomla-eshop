2fe3d7591c56939786ba37bd842053cf2220d322
Serhii Shylo <serhii.shylo@mustpay.tech>
2022-03-03 10:51:24 +0200

    First release

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# joomla-eshop
-ConcordPay plugin for Joomla EShop
+# Модуль ConcordPay для Joomla EShop
+
+Creator: [ConcordPay](https://concordpay.concord.ua)<br>
+Tags: ConcordPay, Joomla, EShop, payment, payment gateway, credit card, Visa, Masterсard, Apple Pay, Google Pay<br>
+Requires at least: Joomla 3.8, EShop 3.3<br>
+License: GNU GPL v3.0<br>
+License URI: [License](https://opensource.org/licenses/GPL-3.0)
+
+Этот модуль позволит вам принимать платежи через платёжную систему **ConcordPay**.
+
+Для работы модуля у вас должны быть установлены **CMS Joomla 3.x** и модуль электронной коммерции **EShop 3.x**.
+
+**Обратите внимание!**<br>
+Модуль не является стандартным плагином **Joomla**, а представляет собой платёжный плагин к компоненту **EShop**, требующий иного способа установки.
+
+## Установка
+
+### Установка через загрузку модуля
+
+1. В административной части сайта перейти в *«Components -> EShop -> Plugins -> Payments»* и в разделе *«Install New Payment Plugin»* загрузить архив с модулем,
+   который находится в папке `package`.
+
+2. Скопировать файл `concordpayorderstatus.php` из папки `package` в папку `administrator/components/com_eshop/elements`.
+
+3. Зайти в настройки плагина **os_concordpay**.
+
+4. Установить необходимые настройки плагина.<br>
+   Состояние: **Включено**<br>
+
+   Указать данные, полученные от платёжной системы:
+    - *Идентификатор продавца (Merchant ID)*;
+    - *Секретный ключ (Secret Key)*.
+
+   Также установить статусы заказов на разных этапах их существования.
+
+5. Сохранить настройки модуля.
+
+Модуль готов к работе.
+
+### Деинсталляция
+
+1. В административной части сайта в разделе *«Components -> EShop -> Plugins -> Payments»* найти плагин **os_concordpay**.
+
+2. В столбце *Status* возле модуля выбрать в выпадающем списке операцию *Delete* и удалить плагин.
+
+3. Вручную удалить файл `administrator/components/com_eshop/elements/concordpayorderstatus.php`.
+
+*Модуль Joomla EShop протестирован для работы с Joomla 4.10.0, EShop 3.5.2 и PHP 7.4.*
---
+++ os_concordpay.php
@@ -0,0 +1,247 @@
+<?php
+
+// No direct access
+defined('_JEXEC') or die();
+
+// API service connection.
+require_once 'ConcordPayApi.php';
+
+/**
+ * PHP version 7.2.34
+ *
+ * @category   Class
+ * @package    EShop
+ * @subpackage EShop
+ * @author     ConcordPay <serhii.shylo@mustpay.tech>
+ * @copyright  2022 ConcordPay
+ * @license    GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ * @link       https://concordpay.concord.ua
+ * @since      3.8.0
+ */
+class os_concordpay extends os_payment
+{
+	// Order ID by table #__eshop_orderstatusdetails.
+	public const CONCORDPAY_ORDER_STATUS_REFUNDED = 11;
+
+	/**
+	 * @var ConcordPayApi
+	 *
+	 * @since version 3.8.0
+	 */
+	private $concordpay;
+
+	/**
+	 * @var string
+	 *
+	 * @since version 3.8.0
+	 */
+	private $merchantId;
+
+	/**
+	 * @var string
+	 * @since version 3.8.0
+	 */
+	private $secretKey;
+
+	/**
+	 * @var string
+	 * @since version 3.8.0
+	 */
+	private $language;
+
+	/**
+	 * Class constructor.
+	 *
+	 * @param $params
+	 *
+	 * @since version 3.8.0
+	 */
+	public function __construct($params)
+	{
+		$this->merchantId = $params->get('merchant_id');
+		$this->secretKey  = $params->get('secret_key');
+		$this->language   = $params->get('language');
+
+		$this->concordpay = new ConcordPayApi($this->secretKey);
+
+		$config = array(
+			'type' => 0,
+			'show_card_type' => false,
+			'show_card_holder_name' => false
+		);
+		parent::__construct($params, $config);
+	}
+
+	/**
+	 * Payment processing.
+	 *
+	 * @param   array $data Order info.
+	 *
+	 * @return void
+	 *
+	 * @since 3.8.0
+	 */
+	public function processPayment($data)
+	{
+		$this->url = $this->concordpay->getApiUrl();
+
+		$siteUrl   = JUri::root();
+		$orderId   = $data['order_id'];
+		$firstname = $data['firstname'];
+		$lastname  = $data['lastname'];
+		$phone     = $data['telephone'];
+		$email     = $data['email'];
+
+		$description = 'Оплата картой на сайте ' . htmlspecialchars($_SERVER['HTTP_HOST'])
+			. " , $firstname $lastname, $phone.";
+
+		// TODO: Разработка модуля производилась на версии EShop 3.3.0, где присутствует ошибка,
+		//  из-за которой возврат пользователя на сайт магазина после успешной оплаты приводит к выводу
+		//  сообщения "Order is not existed!".
+		//  Данный момент надо проверить на более новой версии EShop для параметра $approveUrl.
+		$approveUrl  = $siteUrl . 'index.php?option=com_eshop&view=checkout&layout=complete' . $orderId;
+		$declineUrl  = $siteUrl . 'index.php?option=com_eshop&view=checkout&layout=cancel&id=' . $orderId;
+		$cancelUrl   = $siteUrl . 'index.php?option=com_eshop&view=checkout&layout=cancel&id=' . $orderId;
+		$callbackUrl = $siteUrl . 'index.php?option=com_eshop&task=checkout.verifyPayment&payment_method=os_concordpay&type=notify';
+
+		$orderInfo = [
+			'operation'    => 'Purchase',
+			'merchant_id'  => $this->merchantId,
+			'amount'       => $data['total'],
+			'order_id'     => $orderId,
+			'currency_iso' => $data['currency_code'],
+			'description'  => $description,
+			'approve_url'  => $approveUrl,
+			'decline_url'  => $declineUrl,
+			'cancel_url'   => $cancelUrl,
+			'callback_url' => $callbackUrl,
+			// Statistics.
+			'client_last_name'  => $firstname,
+			'client_first_name' => $lastname,
+			'phone' => $phone,
+			'email' => $email,
+		];
+
+		$orderInfo['signature'] = $this->concordpay->getRequestSignature($orderInfo);
+
+		$this->submitPost($this->url, $orderInfo);
+	}
+
+	/**
+	 * Process payment
+	 *
+	 * @return boolean
+	 *
+	 * @since 3.8.0
+	 */
+	public function verifyPayment()
+	{
+		$data = json_decode(file_get_contents('php://input'), true);
+		$settings = [
+			'merchant_id' => $this->merchantId,
+		];
+		$isPaymentValid = $this->concordpay->isPaymentValid($data, $settings);
+
+		if ($isPaymentValid)
+		{
+			$errorMessages = '';
+
+			// Check amount.
+			$amount = $data['amount'] ?? '';
+
+			if (empty($amount))
+			{
+				$errorMessages .= 'Error: Amount is incorrect.' . PHP_EOL;
+			}
+
+			// Check currency.
+			$currency = $data['currency'] ?? '';
+
+			if (empty($currency))
+			{
+				$errorMessages .= 'Error: Currency is incorrect.' . PHP_EOL;
+			}
+
+			// Check operation type.
+			$type = $data['type'] ?? '';
+
+			if (empty($type) || !in_array($type, $this->concordpay->getAllowedOperationTypes(), true))
+			{
+				$errorMessages .= 'Error: Unknown operation type.' . PHP_EOL;
+			}
+
+			/** @var OrderEshop $order */
+			$order = JTable::getInstance('Eshop', 'Order');
+
+			// Check order existing.
+			$id = (int) $data['orderReference'];
+
+			if (!$order->load($id))
+			{
+				$errorMessages .= 'Error: Order not found.' . PHP_EOL;
+			}
+
+			// Re-paid check.
+			if ($data['type'] !== ConcordPayApi::RESPONSE_TYPE_REVERSE
+				&& (int) $order->order_status_id === (int) $this->params->get('status_approved')
+			)
+			{
+				$errorMessages .= 'Error: Order already completed.' . PHP_EOL;
+			}
+
+			// Terminate and show errors.
+			if (!empty($errorMessages))
+			{
+				exit($errorMessages);
+			}
+
+			$statusApproved = empty($this->params->get('status_approved'))
+				? (int) EshopHelper::getConfigValue('complete_status_id')
+				: (int) $this->params->get('status_approved');
+			$statusDeclined = empty($this->params->get('status_declined'))
+				? (int) EshopHelper::getConfigValue('canceled_status_id')
+				: (int) $this->params->get('status_declined');
+			$statusRefunded = empty($this->params->get('status_refunded'))
+				? self::CONCORDPAY_ORDER_STATUS_REFUNDED
+				: (int) $this->params->get('status_refunded');
+
+			// Update order status.
+			if ($data['transactionStatus'] === ConcordPayApi::TRANSACTION_STATUS_APPROVED)
+			{
+				// Ordinary payment.
+				if ($data['type'] === ConcordPayApi::RESPONSE_TYPE_PAYMENT)
+				{
+					$order->transaction_id  = $data['transactionId'];
+					$order->order_status_id = $statusApproved;
+					$order->store();
+
+					EshopHelper::completeOrder($order);
+					JPluginHelper::importPlugin('eshop');
+					$dispatcher = JDispatcher::getInstance();
+					$dispatcher->trigger('onAfterCompleteOrder', array($order));
+
+					// Send confirmation email here
+					if (EshopHelper::getConfigValue('order_alert_mail'))
+					{
+						EshopHelper::sendEmails($order);
+					}
+
+					exit('Payment OK');
+				}
+
+				// Refunded payment.
+				if ($data['type'] === ConcordPayApi::RESPONSE_TYPE_REVERSE)
+				{
+					$order->transaction_id  = $data['transactionId'];
+					$order->order_status_id = $statusRefunded;
+					$order->comment = 'Payment successfully refunded. ConcordPay transaction ID: ' . $order->transaction_id;
+					$order->store();
+
+					exit('Refunded OK');
+				}
+			}
+		}
+
+		return false;
+	}
+}
